56a8edde303fbb9334a973b614532da08c7e0633
NifleySnifley <sailzforjunkmailz@gmail.com>
2023-11-23 22:35:45 -0600

    Driver and testing for rangefinders

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "pico/time.h"
 #include <stdint.h>
 #include <math.h>
+#include "hardware/i2c.h"
 
 #define LED_R 21
 #define LED_Y 20
@@ -27,6 +28,17 @@
 
 #define IMU_SDA 14
 #define IMU_SCL 15
+#define IMU_I2C i2c1
+
+#define LIDAR_SDA 8
+#define LIDAR_SCL 9
+#define LIDAR_I2C i2c0
+// white
+#define LIDAR_0_INT 6
+#define LIDAR_0_CE 16
+// green
+#define LIDAR_1_INT 7
+#define LIDAR_1_CE 17
 
 const float MOTOR_KHZ = 30.f;
 const int MOTOR_CYCLES = 4096;
@@ -186,6 +198,185 @@ public:
     }
 };
 
+#define VL6180X_I2C_SLAVE_DEVICE_ADDRESS 0x212
+// TODO: Put CE up on one, change I2C address, 
+class VL6180X {
+    i2c_inst_t* bus;
+    int pin_CE;
+
+public:
+    uint8_t addr = 0x29;
+
+    VL6180X(i2c_inst_t* i2c, int CE, int address = 0x29) : pin_CE(CE), bus(i2c), addr(address) {
+        gpio_init(pin_CE);
+        gpio_set_dir(pin_CE, GPIO_OUT);
+    }
+
+    void power(bool pow) {
+        gpio_put(pin_CE, pow);
+    }
+
+    uint16_t frac16_encode(float v) {
+        return (uint8_t)roundf(v * (1 << 7));
+    }
+
+    uint8_t frac8_encode(float v) {
+        return (uint8_t)roundf(v * (1 << 4));
+    }
+
+    float frac8_decode(uint8_t v) {
+        return (float)v / (float)(1 << 4);
+    }
+
+    float frac16_decode(uint16_t v) {
+        return (float)v / (float)(1 << 7);
+    }
+
+    void reg_write8(uint16_t regn, uint8_t val) {
+        uint8_t buf[] = {
+            (uint8_t)(regn >> 8),
+            (uint8_t)(regn >> 0),
+            val
+        };
+        i2c_write_blocking(bus, addr, buf, sizeof(buf), false);
+    }
+
+    void reg_write16(uint16_t regn, uint16_t val) {
+        uint8_t buf[] = {
+                   (uint8_t)(regn >> 8),
+                    (uint8_t)(regn & 0xFF),
+                   (uint8_t)((val & 0xFF00) >> 8),
+                   (uint8_t)(val & 0xFF)
+        };
+        i2c_write_blocking(bus, addr, buf, sizeof(buf), false);
+    }
+
+    uint8_t reg_read8(uint16_t regn) {
+        uint8_t txbuf[] = {
+            (uint8_t)(regn >> 8),
+            (uint8_t)(regn & 0xFF)
+        };
+        uint8_t rxbuf[] = { 0x00 };
+
+        i2c_write_blocking(bus, addr, txbuf, sizeof(txbuf), true);
+        i2c_read_blocking(bus, addr, rxbuf, sizeof(rxbuf), false);
+        return rxbuf[0];
+    }
+
+    uint16_t reg_read16(uint16_t regn) {
+        uint8_t txbuf[] = { (uint8_t)(regn >> 8),
+            (uint8_t)(regn & 0xFF) };
+        uint8_t rxbuf[] = { 0x00,0x00 };
+
+        i2c_write_blocking(bus, addr, txbuf, sizeof(txbuf), true);
+        i2c_read_blocking(bus, addr, rxbuf, sizeof(rxbuf), false);
+        return (uint16_t)(rxbuf[0]) || ((uint16_t)rxbuf[1] << 8); // ???
+    }
+
+    void init() {
+        reg_write8(0x207, 0x01);
+        reg_write8(0x208, 0x01);
+        reg_write8(0x096, 0x00);
+        reg_write8(0x097, 0xFD); // RANGE_SCALER = 253
+        reg_write8(0x0E3, 0x01);
+        reg_write8(0x0E4, 0x03);
+        reg_write8(0x0E5, 0x02);
+        reg_write8(0x0E6, 0x01);
+        reg_write8(0x0E7, 0x03);
+        reg_write8(0x0F5, 0x02);
+        reg_write8(0x0D9, 0x05);
+        reg_write8(0x0DB, 0xCE);
+        reg_write8(0x0DC, 0x03);
+        reg_write8(0x0DD, 0xF8);
+        reg_write8(0x09F, 0x00);
+        reg_write8(0x0A3, 0x3C);
+        reg_write8(0x0B7, 0x00);
+        reg_write8(0x0BB, 0x3C);
+        reg_write8(0x0B2, 0x09);
+        reg_write8(0x0CA, 0x09);
+        reg_write8(0x198, 0x01);
+        reg_write8(0x1B0, 0x17);
+        reg_write8(0x1AD, 0x00);
+        reg_write8(0x0FF, 0x05);
+        reg_write8(0x100, 0x05);
+        reg_write8(0x199, 0x05);
+        reg_write8(0x1A6, 0x1B);
+        reg_write8(0x1AC, 0x3E);
+        reg_write8(0x1A7, 0x1F);
+        reg_write8(0x030, 0x00);
+        reg_write8(0x016, 0x00);
+
+        // readout__averaging_sample_period = 48
+        reg_write8(0x10A, 0x30);
+        // sysals__analogue_gain_light = 6 (ALS gain = 1 nominal, actually 1.01 according to table "Actual gain values" in datasheet)
+        reg_write8(0x03F, 0x46);
+        // sysrange__vhv_repeat_rate = 255 (auto Very High Voltage temperature recalibration after every 255 range measurements)
+        reg_write8(0x031, 0xFF);
+        // sysals__integration_period = 99 (100 ms)
+        reg_write16(0x040, 0x0063);
+        // sysrange__vhv_recalibrate = 1 (manually trigger a VHV recalibration)
+        reg_write8(0x02E, 0x01);
+        // sysrange__intermeasurement_period = 0 (10 ms)
+        reg_write8(0x03E, 0x00);
+        // sysals__intermeasurement_period = 49 (500 ms)
+        reg_write8(0x03E, 0x31);
+        // als_int_mode = 4 (ALS new sample ready interrupt); range_int_mode = 4 (range new sample ready interrupt)
+        reg_write8(0x014, 0x24);
+        // Reset other settings to power-on defaults
+        // sysrange__max_convergence_time = 49 (49 ms)
+        reg_write8(0x01C, 0x31);
+        // disable interleaved mode
+        reg_write8(0x2A3, 0);
+    }
+
+    void start_measuring() {
+        int period = 50;
+        int16_t period_reg = clamp(0, 254, (int16_t)(period / 10) - 1);
+        reg_write8(0x01B, period_reg);
+
+        // SYSRANGE__START
+        reg_write8(0x018, 0x03);
+    }
+
+    int get_range_mm() {
+        uint8_t range = reg_read8(0x062);
+        reg_write8(0x015, 0x01);
+
+        return (int)range;
+    }
+};
+
+// Just a container/subsystem, not really any sort of abstraction
+class DistanceSensors {
+public:
+    VL6180X white;
+    VL6180X green;
+
+    DistanceSensors() : white(LIDAR_I2C, LIDAR_0_CE, 0x29), green(LIDAR_I2C, LIDAR_1_CE, 0x29) {
+
+    }
+
+    void init() {
+        white.power(true);
+        green.power(false);
+
+        white.addr = 0x29;
+        white.reg_write8(VL6180X_I2C_SLAVE_DEVICE_ADDRESS, 0x30 & 0x7F);
+        white.addr = 0x30;
+        white.reg_write8(VL6180X_I2C_SLAVE_DEVICE_ADDRESS, 0x30 & 0x7F);
+
+        green.power(true);
+        green.addr = 0x29;
+        green.reg_write8(VL6180X_I2C_SLAVE_DEVICE_ADDRESS, 0x30 & 0x7F);
+
+        green.init();
+        white.init();
+
+        green.start_measuring();
+        white.start_measuring();
+    }
+};
+
 MotorController MA(MOT_IA1, MOT_IA2);
 MotorController MB(MOT_IB2, MOT_IB1);
 Encoder EB(ENC_A_B, ENC_A_A, ENC_COUNT2ROT_FAC);
@@ -193,6 +384,10 @@ Encoder EA(ENC_B_A, ENC_B_B, ENC_COUNT2ROT_FAC);
 
 PIDController pa(5.0f, 0.5f, 0.2f);//(2.0f, 0.5f, 0.2f);
 PIDController pb(5.0f, 0.5f, 0.2f);//(2.0f, 0.5f, 0.2f);
+
+bool reserved_addr(uint8_t addr) {
+    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
 
 int main() {
     stdio_init_all();
@@ -207,13 +402,66 @@ int main() {
 
     pa.reset();
 
-    while (true) {
-        //0.5 * (EA.get() + EB.get());// Fun force feedbackish!
-        float tgt = time_seconds() * 2.0f;
-        float out_a = pa.calculate(EA.get(), tgt);
-        float out_b = pb.calculate(EB.get(), tgt);
-        MA.set(out_a);
-        MB.set(out_b);
-        printf("%.3f, %.3f\n", EA.get(), EB.get());
+    MA.set(0.0f);
+    MB.set(0.0f);
+
+    // while (true) {
+        // //0.5 * (EA.get() + EB.get());// Fun force feedbackish!
+        // float tgt = time_seconds() * 2.0f;
+        // float out_a = pa.calculate(EA.get(), tgt);
+        // float out_b = pb.calculate(EB.get(), tgt);
+        // MA.set(out_a);
+        // MB.set(out_b);
+        // printf("%.3f, %.3f\n", EA.get(), EB.get());
+    // }
+
+    // gpio_put(LIDAR_0_CE, 0); // Select!
+    // gpio_put(LIDAR_1_CE, 1);
+
+    i2c_init(LIDAR_I2C, 100 * 1000);
+    gpio_set_function(LIDAR_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(LIDAR_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(LIDAR_SDA);
+    gpio_pull_up(LIDAR_SCL);
+
+    DistanceSensors ds;
+    ds.init();
+
+
+    while (1) {
+
+
+        // printf("\nI2C Bus Scan\n");
+        // printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+        // for (int addr = 0; addr < (1 << 7); ++addr) {
+        //     if (addr % 16 == 0) {
+        //         printf("%02x ", addr);
+        //     }
+
+        //     // Perform a 1-byte dummy read from the probe address. If a slave
+        //     // acknowledges this address, the function returns the number of bytes
+        //     // transferred. If the address byte is ignored, the function returns
+        //     // -1.
+
+        //     // Skip over any reserved addresses.
+        //     int ret;
+        //     uint8_t rxdata;
+        //     if (reserved_addr(addr))
+        //         ret = -1;
+        //     else
+        //         ret = i2c_read_blocking(LIDAR_I2C, addr, &rxdata, 1, false);
+
+        //     printf(ret < 0 ? "." : "@");
+        //     printf(addr % 16 == 15 ? "\n" : "  ");
+        // }
+
+        // // printf("Addr reg: %d\n", ds.white.reg_read8(VL6180X_I2C_SLAVE_DEVICE_ADDRESS));
+        // // ds.white.reg_write8(VL6180X_I2C_SLAVE_DEVICE_ADDRESS, 0x30);
+        // // printf("Write\n");
+
+        printf("%d, %d\n", ds.green.get_range_mm(), ds.white.get_range_mm());
+
+        // printf("Done.\n");
+        sleep_ms(10);
     }
 }
